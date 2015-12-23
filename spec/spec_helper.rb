d83f479e463e7b07e3adb7e11d1441f80df7b5be
Lessan Vaezi <lessan@gmail.com>
2015-12-23 10:21:44 +0800

    Add support for RSpec :focus keyword

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,13 @@
 require 'simplecov'
+# Start the code coverage inspector here so any tests that are run
+# which include the spec_helper count towards the code that is covered.
 SimpleCov.start
+
+RSpec.configure do |config|
+  # These two settings work together to allow you to limit a spec run
+  # to individual examples or groups you care about by tagging them with
+  # `:focus` metadata. When nothing is tagged with `:focus`, all examples
+  # get run.
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+end
