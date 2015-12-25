3fc6efec80d615f73af38885f6da7346bd313626
Lessan Vaezi <lessan@gmail.com>
2015-12-25 11:32:11 +0800

    Add methods to Compass for converting an angle to a direction and v.v.

--- lib/compass.rb
+++ lib/compass.rb
@@ -2,4 +2,20 @@ module Compass
   def self.validate_direction(direction)
     %w(north south east west).include? direction.to_s.downcase
   end
+
+  def self.angle_from(direction)
+    return 0   if direction == 'north'
+    return 90  if direction == 'east'
+    return 180 if direction == 'south'
+    return 270 if direction == 'west'
+    fail ArgumentError, "Invalid direction: #{direction.inspect}"
+  end
+
+  def self.direction_from(angle)
+    return 'north' if angle == 0
+    return 'east'  if angle == 90
+    return 'south' if angle == 180
+    return 'west'  if angle == 270
+    fail ArgumentError, "Invalid angle: #{angle.inspect}"
+  end
 end
--- spec/compass_spec.rb
+++ spec/compass_spec.rb
@@ -25,5 +25,87 @@ describe Compass do
       it { is_expected.to be false }
     end
   end
-end
 
+  context 'getting an angle from a direction' do
+    subject { described_class.angle_from(direction) }
+
+    context 'with north' do
+      let(:direction) { 'north' }
+      it { is_expected.to eq(0) }
+    end
+
+    context 'with east' do
+      let(:direction) { 'east' }
+      it { is_expected.to eq(90) }
+    end
+
+    context 'with south' do
+      let(:direction) { 'south' }
+      it { is_expected.to eq(180) }
+    end
+
+    context 'with west' do
+      let(:direction) { 'west' }
+      it { is_expected.to eq(270) }
+    end
+
+    context 'with an invalid direction' do
+      let(:direction) { 'NORTH' }
+      it 'raises an ArgumentError' do
+        expect { subject }.to raise_error(ArgumentError)
+      end
+    end
+
+    context 'with a nil direction' do
+      let(:direction) { nil }
+      it 'raises an ArgumentError' do
+        expect { subject }.to raise_error(ArgumentError)
+      end
+    end
+  end
+
+  context 'getting a direction from an angle' do
+    subject { described_class.direction_from(angle) }
+
+    context 'with 0' do
+      let(:angle) { 0 }
+      it { is_expected.to eq('north') }
+    end
+
+    context 'with 90' do
+      let(:angle) { 90 }
+      it { is_expected.to eq('east') }
+    end
+
+    context 'with 180' do
+      let(:angle) { 180 }
+      it { is_expected.to eq('south') }
+    end
+
+    context 'with 270' do
+      let(:angle) { 270 }
+      it { is_expected.to eq('west') }
+    end
+
+    context 'with an invalid angle' do
+      let(:angle) { 1 }
+      it 'raises an ArgumentError' do
+        expect { subject }.to raise_error(ArgumentError)
+      end
+    end
+
+    context 'with a string as an angle' do
+      let(:angle) { '90' }
+      it 'raises an ArgumentError' do
+        expect { subject }.to raise_error(ArgumentError)
+      end
+    end
+
+    context 'with a nil angle' do
+      let(:angle) { nil }
+      it 'raises an ArgumentError' do
+        expect { subject }.to raise_error(ArgumentError)
+      end
+    end
+  end
+end
