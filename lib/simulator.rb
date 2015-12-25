25b9d555e50a60cce2976197b91f0e63239ff75c
Lessan Vaezi <lessan@gmail.com>
2015-12-25 10:41:28 +0800

    Update specs for Simulator class

--- lib/simulator.rb
+++ lib/simulator.rb
@@ -1,5 +1,18 @@
+require 'command_parser'
+require 'robot'
+require 'table'
+require 'navigator'
+
 class Simulator
-  def self.process(_commands)
-    puts 'To be implemented'
+  attr_reader :robot
+
+  def initialize
+    @robot = Robot.new
+  end
+
+  def process(command_string)
+    command_parser = CommandParser.new(command_string)
+    command = command_parser.parse
+    command.execute(robot)
   end
 end
--- spec/simulator_spec.rb
+++ spec/simulator_spec.rb
@@ -2,7 +2,36 @@ require 'spec_helper'
 require 'simulator'
 
 describe Simulator do
-  it 'processes commands' do
-    described_class.process('commands')
+  it 'initializes a Robot and sets it as an instance variable exposed via an attribute' do
+    robot = double
+    expect(Robot).to receive(:new).and_return(robot)
+    simulator = described_class.new
+    expect(simulator.robot).to eq(robot)
+  end
+
+  context 'processing a command string' do
+    let(:simulator) { described_class.new }
+    let(:command_string) { 'some string' }
+    let(:command_parser) { double }
+    let(:command) { double.as_null_object }
+    before(:each) do
+      allow(CommandParser).to receive(:new).and_return(command_parser)
+      allow(command_parser).to receive(:parse).and_return(command)
+    end
+
+    it 'creates a CommandParser and passes it the command string' do
+      expect(CommandParser).to receive(:new).with(command_string).and_return(command_parser)
+      simulator.process(command_string)
+    end
+
+    it 'calls parse on the CommandParser instance' do
+      expect(command_parser).to receive(:parse).and_return(double.as_null_object)
+      simulator.process(command_string)
+    end
+
+    it 'executes the command passing it the robot' do
+      expect(command).to receive(:execute).with(simulator.robot)
+      simulator.process(command_string)
+    end
   end
 end
