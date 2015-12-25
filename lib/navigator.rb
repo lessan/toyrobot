f9bfd2ff211b969e3606e39ba1f7fb0f3d30aeb1
Lessan Vaezi <lessan@gmail.com>
2015-12-25 12:08:09 +0800

    Add a placed? method to the Navigator for determining if an object is placed on the surface

--- lib/navigator.rb
+++ lib/navigator.rb
@@ -1,9 +1,24 @@
 require 'type_checker'
+require 'compass'
+require 'direction_matrix'
 
 class Navigator
   attr_reader :object
 
+  # object must conform to the interface:
+  #   x      get/set integer
+  #   y      get/set integer
+  #   angle  get/set integer
   def initialize(object)
     @object = object
+  end
+
+  def placed?
+    # we can assume an object is "placed" on the surface if it has
+    # a valid position and an angle set
+    return false unless TypeChecker.integer?(object.x)
+    return false unless TypeChecker.integer?(object.y)
+    return false unless TypeChecker.integer?(object.angle)
+    true
   end
 end
--- spec/navigator_spec.rb
+++ spec/navigator_spec.rb
@@ -9,4 +9,33 @@ describe Navigator do
       expect(navigator.object).to eq(object)
     end
   end
+
+  context 'determining if an object is "placed" on a surface' do
+    let(:x) { double }
+    let(:y) { double }
+    let(:angle) { double }
+    let(:navigator) { described_class.new(double(x: x, y: y, angle: angle)) }
+    before(:each) do
+      allow(TypeChecker).to receive(:integer?).and_return(true)
+    end
+
+    it 'returns true if all type checks pass' do
+      expect(navigator.placed?).to be(true)
+    end
+
+    it 'checks the type of x is an integer and returns false if it isnt' do
+      expect(TypeChecker).to receive(:integer?).with(x).and_return(false)
+      expect(navigator.placed?).to be(false)
+    end
+
+    it 'checks the type of y is an integer and returns false if it isnt' do
+      expect(TypeChecker).to receive(:integer?).with(y).and_return(false)
+      expect(navigator.placed?).to be(false)
+    end
+
+    it 'checks the angle of x is an integer and returns false if it isnt' do
+      expect(TypeChecker).to receive(:integer?).with(angle).and_return(false)
+      expect(navigator.placed?).to be(false)
+    end
+  end
 end
