60ef270cc5d44db91f7428075106770df4374648
Lessan Vaezi <lessan@gmail.com>
2015-12-25 11:44:49 +0800

    Improve DirectionMatrix spec

--- spec/direction_matrix_spec.rb
+++ spec/direction_matrix_spec.rb
@@ -3,9 +3,9 @@ require 'direction_matrix'
 
 describe DirectionMatrix do
   it 'provides a method for each direction' do
-    expect(described_class).to respond_to(:north)
-    expect(described_class).to respond_to(:east)
-    expect(described_class).to respond_to(:south)
-    expect(described_class).to respond_to(:west)
+    expect(described_class.north).to eq [0,1]
+    expect(described_class.east).to eq [1,0]
+    expect(described_class.south).to eq [0,-1]
+    expect(described_class.west).to eq [-1,0]
   end
 end
